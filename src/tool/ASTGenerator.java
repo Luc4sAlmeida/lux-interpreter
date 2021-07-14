685d5833a16b6435ce1f5c98f046537c7e4f8e11
Lucas Almeida <lucasalmeida3_@hotmail.com>
2021-07-14 10:45:53 -0300

    Implemented statements

--- src/tool/ASTGenerator.java
+++ src/tool/ASTGenerator.java
@@ -18,6 +18,10 @@ class ASTGenerator {
                 "Literal  : Object value",
                 "Unary    : Token operator, Expr right"
         ));
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "Expression : Expr expression",
+                "Print      : Expr expression"
+        ));
     }
 
     private static void defineAst(
